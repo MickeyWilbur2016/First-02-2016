aeb64050597352d9e1f72ab28232394825eca708
MickeyWilbur2016 <mickeywilbur@yahoo.com>
2020-02-13 22:07:10 -0600

    added some text in the print statements

--- beginning/first.py
+++ beginning/first.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
     if i % 5 == 0 and i%3 == 0:
-        print "FizBuzz"
+        print "{} is FizBuzz".format(i)
     elif i % 5 == 0:
-        print "Fizz"
+        print "{} is Fizz".format(i)
     elif i % 3 == 0:
-        print "Buzz"
+        print "{} is Buzz".format(i)
     else:
-        print i +"not fiz or buzz and definitely not FizzBuzz"
+        print "{} is not fiz or buzz and definitely not FizzBuzz".format(i)
